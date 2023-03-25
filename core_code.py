768384ee1804f1ddae3c9342e5fc520fd8aa6ca2
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-03-25 16:21:55 +0530

    Update core_code.py

--- core_code.py
+++ core_code.py
@@ -17,13 +17,13 @@ from IPython.external.path import path as Path
 
 data_csv = Path(__file__).parents[1] / 'IPYNB File/data.csv'
 
-dir_name = os.path.abspath(os.path.dirname(data_csv))
-location = os.path.join(dir_name, 'data.csv')
+# dir_name = os.path.abspath(os.path.dirname(data_csv))
+# location = os.path.join(dir_name, 'data.csv')
 data_locations = pd.read_csv(location)
 
 ratings_csv = Path(__file__).parents[1] / 'IPYNB File/ratings.csv'
-dir_name2 = os.path.abspath(os.path.dirname(ratings_csv))
-location2 = os.path.join(dir_name, 'ratings.csv')
+# dir_name2 = os.path.abspath(os.path.dirname(ratings_csv))
+# location2 = os.path.join(dir_name, 'ratings.csv')
 ratings_locations=pd.read_csv(location2)
 
 
