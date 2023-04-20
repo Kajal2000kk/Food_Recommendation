7a71c537b09016ff2183b8cd044defd1deb4c1f7
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-04-20 02:45:20 -1200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ t = test2.result()
 
 foods = t.food()
 
+st.write("check out this [link](https://diet-recommendation-system.streamlit.app/Diet_Recommendation)")
+
+st.write("# Welcome to Diet Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on my [repo]#(https://github.com/zakaria-narjis/Diet-Recommendation-System).
+    """
 import base64
 
 @st.cache(allow_output_mutation=True)
