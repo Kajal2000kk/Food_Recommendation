5ab116737bb93dc05ddb0dde8c66d4137ff14b32
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-04-22 05:05:57 -1200

    Update main_page.py

--- streamlit_app.py/pages/main_page.py
+++ streamlit_app.py/pages/main_page.py
@@ -1,5 +1,15 @@
 import base64
 import streamlit as st
+
+st.sidebar.write("click [recommendation-diet](https://github.com/Kajal2000kk/Food_Recommendation)")
+st.write("# Welcome to Diet Recommendation System! 👋")
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on my [repo](https://github.com/Kajal2000kk/Food_Recommendation).
+    """)
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
@@ -15,15 +25,7 @@ def set_png_as_page_bg(png_file):
         background-size: cover;
     }
     </style>
-st.sidebar.write("click [recommendation-diet](https://github.com/Kajal2000kk/Food_Recommendation)")
-st.write("# Welcome to Diet Recommendation System! 👋")
-st.sidebar.success("Select a recommendation app.")
 
-st.markdown(
-    """
-    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
-    You can find more details and the whole project on my [repo](https://github.com/Kajal2000kk/Food_Recommendation).
-    """)
   ''' % bin_str
     
     st.markdown(page_bg_img, unsafe_allow_html=True)
