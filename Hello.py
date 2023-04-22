5a58c2a3a60ef465d2973aa1ecf89b543164a89b
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-04-23 00:52:33 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,27 +1,5 @@
 import streamlit as st
 from streamlit.logger import get_logger
-import base64
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_jpg_as_page_bg(jpg_file):
-    bin_str = get_base64_of_bin_file(jpg_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-        background-image: url("data:image/jpg;base64,%s");
-        background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_jpg_as_page_bg('imgs/img4.jpg')
 
 LOGGER = get_logger(__name__)
 def run():
