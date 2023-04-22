f495306c329c8622163df140891c95a86f10a4d2
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-04-22 02:38:59 -1200

    Update main_page.py

--- streamlit_app.py/pages/main_page.py
+++ streamlit_app.py/pages/main_page.py
@@ -7,18 +7,18 @@ def main_page():
     st.markdown("# Main page 🎈")
     st.sidebar.markdown("# Main page 🎈")
 
-def page2():
+def page_2():
     st.markdown("# Page_2 ❄️")
     st.sidebar.markdown("# Page_2 ❄️")
 
-def page3():
+def page_3():
     st.markdown("# Page 3 🎉")
     st.sidebar.markdown("# Page 3 🎉")
 
 page_names_to_funcs = {
     "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
+    "Page_2": page2,
+    "Page_3": page3,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
