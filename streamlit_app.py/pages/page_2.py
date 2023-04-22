fd90681dabcf77460be8f2adbee94853fcb0b42c
Kajal2000kk <89658792+Kajal2000kk@users.noreply.github.com>
2023-04-22 03:00:01 -1200

    Update page_2.py

--- streamlit_app.py/pages/page_2.py
+++ streamlit_app.py/pages/page_2.py
@@ -3,7 +3,9 @@ import test2
 from PIL import Image
 import base64
 st.set_page_config(page_title="Automatic Diet Recommendation", page_icon="💪",layout="wide")
-
+# def page_2():
+st.markdown("# Page_2 ❄️")
+st.sidebar.markdown("# Page_2 ❄️")
 t = test2.result()
 # st.sidebar.write("click [recommendation-diet](https://Kajal2000kk/Food_Recommendation)")
 # st.write("# Welcome to Diet Recommendation System! 👋")
